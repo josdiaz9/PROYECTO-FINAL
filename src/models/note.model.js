c4015f8da3fd32e5ad340c2707dbf100b7a8216a
zeke.dev <josdiaz@frba.utn.edu.ar>
2026-06-21 23:24:13 -0300

    primer version del proyecto

---
+++ src/models/note.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const noteSchema = new mongoose.Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    contenido: {
+        type: String,
+        required: true
+    },
+    fecha_creacion: {
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+    activo: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
+});
+
+export const NOTE_COLLECTION_NAME = 'Note'
+
+const Note = mongoose.model(NOTE_COLLECTION_NAME, noteSchema)
+
+export default Note
